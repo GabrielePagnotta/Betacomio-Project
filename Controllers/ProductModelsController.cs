7dc2570d12ca7dbebcdc810424eebca5073b267b
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-07-09 21:23:37 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/ProductModelsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Betacomio_Project.Models;
+
+namespace Betacomio_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductModelsController : ControllerBase
+    {
+        private readonly AdventureWorksLt2019Context _context;
+
+        public ProductModelsController(AdventureWorksLt2019Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductModels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductModel>>> GetProductModels()
+        {
+          if (_context.ProductModels == null)
+          {
+              return NotFound();
+          }
+            return await _context.ProductModels.ToListAsync();
+        }
+
+        // GET: api/ProductModels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductModel>> GetProductModel(int id)
+        {
+          if (_context.ProductModels == null)
+          {
+              return NotFound();
+          }
+            var productModel = await _context.ProductModels.FindAsync(id);
+
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            return productModel;
+        }
+
+        // PUT: api/ProductModels/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductModel(int id, ProductModel productModel)
+        {
+            if (id != productModel.ProductModelId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductModels
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductModel>> PostProductModel(ProductModel productModel)
+        {
+          if (_context.ProductModels == null)
+          {
+              return Problem("Entity set 'AdventureWorksLt2019Context.ProductModels'  is null.");
+          }
+            _context.ProductModels.Add(productModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductModel", new { id = productModel.ProductModelId }, productModel);
+        }
+
+        // DELETE: api/ProductModels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductModel(int id)
+        {
+            if (_context.ProductModels == null)
+            {
+                return NotFound();
+            }
+            var productModel = await _context.ProductModels.FindAsync(id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductModels.Remove(productModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductModelExists(int id)
+        {
+            return (_context.ProductModels?.Any(e => e.ProductModelId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Betacomio_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//Db connection service
+builder.Services.AddDbContext<AdventureWorksLt2019Context>(option=>option.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLT2019")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
