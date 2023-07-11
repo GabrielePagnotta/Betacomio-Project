fb64060f9277c43d2397c3f649a546656aee84b2
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-07-11 17:08:28 +0200

    sql connection

---
+++ Controllers/SqlQuery.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+
+namespace Betacomio_Project.Controllers
+{
+    public class SqlQuery
+    {
+        SqlConnection conn = new SqlConnection();
+
+        public SqlQuery() 
+        {
+            try
+            {
+                if(conn.State==System.Data.ConnectionState.Closed)
+                {
+                    conn.ConnectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;TrustServerCertificate=True";
+                }
+
+            }catch (Exception ex) 
+            {
+                Console.WriteLine("si è verificato un errore");
+            }
+        }
+        public string GetHome = "";
+
+        public void Homequery(string _contex)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            
+            
+        }
+    }
+}
