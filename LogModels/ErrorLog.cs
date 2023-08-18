bfe0e48dcdbc7ceb3de5e37e2bbf60f460c5a5cc
AlexCoder99 <alexguci@outlook.it>
2023-08-18 16:13:01 +0200

    Aggiunta DB Context AdminLog (utilizzato per tabella errorLog e tabelle isolate per prove)

---
+++ LogModels/ErrorLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Betacomio_Project.LogModels;
+
+public partial class ErrorLog
+{
+    public int ErrorId { get; set; }
+
+    public int ErrorCode { get; set; }
+
+    public string ErrorDescription { get; set; } = null!;
+
+    public string ErrorOriginClass { get; set; } = null!;
+
+    public string ErrorOriginMethod { get; set; } = null!;
+
+    public DateTime ErrorTime { get; set; }
+
+    public string ErrorMachine { get; set; } = null!;
+}
---
+++ LogModels/UserCredential.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Betacomio_Project.LogModels;
+
+public partial class UserCredential
+{
+    public int UserId { get; set; }
+
+    public string? Username { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public string? BirthYear { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string PasswordSalt { get; set; } = null!;
+
+    public int Nationality { get; set; }
+}
